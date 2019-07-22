eea42bdca796ab4c14c55aefc03d6272480d6519
sergeyk1999 <s.kobrinets@mail.ru>
2019-07-22 14:52:45 +0300

    update

--- integrationtest/Class.cpp
+++ integrationtest/Class.cpp
@@ -42,10 +42,10 @@ void Class::Move(Class* object)
 	//printf("%lf\n", (object->*(x->GetParametr_))());
 	vector <con*> list;
 	list.push_back(x);
-	//list.push_back(y);
+	list.push_back(y);
 	Integrator* a = new Integrator();
 	a->SetDelta(0.1);
-	a->integration(list, 0.5,object);
+	a->integration(list,0, 0.5,object);
 	time_ += a->GetDelta();
 	//cout << "x1=" << object->GetX() << "x2=" << GetX() << "x3=" << (object->*(x->GetParametr_))() << endl;
 	a->~Integrator();
@@ -56,37 +56,5 @@ double Class::fx(double ind,double dep)
 }
 double Class::fy(double ind,double dep)
 {
-	return 3 * time_;
-}
-double Integrator::GetDelta()
-{
-	return delta_;
-}
-void Integrator::SetDelta(double delta)
-{
-	delta_ = delta;
-}
-void Integrator::integration(vector<con*> list, double time,Class* object)
-{
-	double t = 0;
-
-	for (t = 0; t < time; t += delta_)
-	{
-		for (auto i : list)
-		{
-			cout << (object->*(i->GetParametr_))() << endl;
-			(object->*(i->SetParametr_))((object->*(i->GetParametr_))() + stepmaker(object, i,t, (object->*(i->GetParametr_))()));
-		}
-	}
-}
-double Integrator::stepmaker(Class* object,con* i,double ind,double dep)
-{
-	double k1 = delta_ * ((object->*(i->f_))(ind, dep));
-	double k2 = delta_ * ((object->*(i->f_))(ind + delta_ / 2, dep + k1 / 2));
-	double k3 = delta_ * ((object->*(i->f_))(ind + delta_ / 2, dep + k2 / 2));
-	double k4 = delta_ * ((object->*(i->f_))(ind + delta_, dep + k3));
-	//cout << k1 << " " << k2 << " " << k3 << " " << k4 << endl;
-	double res = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-	printf("delta=%lf\n", res);
-	return res;
+	return ind;
 }
--- integrationtest/Class.h
+++ integrationtest/Class.h
@@ -34,15 +34,3 @@ public:
 	f f_=0;
 	con() = default;
 };
-class Integrator
-{
-private:
-	double delta_;
-public:
-	Integrator() = default;
-	~Integrator() = default;
-	void SetDelta(double delta);
-	double GetDelta();
-	void integration(vector <con*> list, double time,Class* object);
-	double stepmaker(Class* object,con* i,double ind,double dep);
-};
